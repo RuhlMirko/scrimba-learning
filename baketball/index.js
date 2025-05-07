4c17b96eb048543f9f1f2a809d54fb1d2b2d32b8
Mirko <ruhlmirkojoel@gmail.com>
2025-05-07 03:29:39 -0300

    Added a visual cue to tell who has the laeding score

--- baketball/index.js
+++ baketball/index.js
@@ -1,17 +1,48 @@
-const homeEL = document.getElementById("home-score");
+const homeEl = document.getElementById("home-score");
 const guestEl = document.getElementById("guest-score");
 
+/* Setting score */
 let homeCount = 0;
 let guestCount = 0;
-homeEL.textContent = homeCount;
+homeEl.textContent = homeCount;
 guestEl.textContent = guestCount;
 
 function addHome(points) {
   homeCount += points;
-  homeEL.textContent = homeCount;
+  homeEl.textContent = homeCount;
+  checkLeader();
 }
 
 function addGuest(points) {
   guestCount += points;
   guestEl.textContent = guestCount;
+  checkLeader();
+}
+
+let maxCount = 0;
+
+function checkLeader() {
+  /*
+  homeEl.classList.remove("leading", "losing");
+  guestEl.classList.remove("leading", "losing");
+  */
+  if (homeCount >= maxCount) {
+    guestEl.classList.remove("leading");
+    guestEl.classList.add("losing");
+
+    maxCount = homeCount;
+
+    homeEl.classList.add("leading");
+    homeEl.classList.remove("losing");
+  }
+  if (guestCount >= maxCount) {
+    homeEl.classList.remove("leading");
+    homeEl.classList.add("losing");
+
+    maxCount = guestCount;
+
+    guestEl.classList.add("leading");
+    guestEl.classList.remove("losing");
+  }
+  console.log(homeCount, guestCount, maxCount);
 }
