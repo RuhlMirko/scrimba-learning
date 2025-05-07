b62c73c19a946f6eaadafaeef7971ae0019f624f
Mirko <ruhlmirkojoel@gmail.com>
2025-05-07 03:55:41 -0300

    Added a timer

--- baketball/index.js
+++ baketball/index.js
@@ -50,4 +50,25 @@ function newGame() {
   guestEl.textContent = guestCount;
   homeEl.classList.remove("leading", "losing");
   guestEl.classList.remove("leading", "losing");
+  startStopwatch();
+}
+
+const timerEl = document.getElementById("timer");
+let startTime;
+let intervalId;
+timerEl.textContent = "00:00";
+
+function startStopwatch() {
+  startTime = Date.now(); // record the start time
+  intervalId = setInterval(() => {
+    const ms = Date.now() - startTime;
+    const totalSeconds = Math.floor(ms / 1000);
+    const minutes = String(Math.floor(totalSeconds / 60)).padStart(2, "0");
+    const seconds = String(totalSeconds % 60).padStart(2, "0");
+    timerEl.textContent = `${minutes}:${seconds}`;
+  }, 1000); // log every second
+}
+
+function stopStopwatch() {
+  clearInterval(intervalId);
 }
