7c56e241b4d85e8563e59ac66c27b17dffec5684
Mirko <ruhlmirkojoel@gmail.com>
2025-05-29 00:14:10 -0300

    Added button to delete specific tweets

--- websites/twiterClone/index.js
+++ websites/twiterClone/index.js
@@ -1,5 +1,4 @@
 // #TODO: add the ability to reply to a specific tweet,
-// Save tweets, likes and retweets to localStorage
 // Allow a user to delete a tweet
 // Customize
 
@@ -37,6 +36,8 @@ document.addEventListener("click", function (e) {
     handleRetweetClick(e.target.dataset.retweet);
   } else if (e.target.dataset.reply) {
     handleReplyClick(e.target.dataset.reply);
+  } else if (e.target.dataset.delete) {
+    deleteTweet(e.target.dataset.delete);
   } else if (e.target.id === "tweet-btn") {
     handleTweetBtnClick();
   }
@@ -55,6 +56,7 @@ function handleTweetBtnClick() {
       replies: [],
       isLiked: false,
       isRetweeted: false,
+      isMine: true,
       uuid: uuidv4(),
     });
     saveTweets();
@@ -69,6 +71,7 @@ function saveTweets() {
 function deleteTweet(uuid) {
   tweetsData = tweetsData.filter((t) => t.uuid !== uuid);
   saveTweets();
+  render();
 }
 
 function handleLikeClick(tweetId) {
@@ -124,53 +127,63 @@ function getFeedHtml() {
     if (tweet.replies.length > 0) {
       tweet.replies.forEach(function (reply) {
         repliesHtml += `
-<div class="tweet-reply">
-    <div class="tweet-inner">
-        <img src="${reply.profilePic}" class="profile-pic">
-            <div>
-                <p class="handle">${reply.handle}</p>
-                <p class="tweet-text">${reply.tweetText}</p>
-            </div>
+        <div class="tweet-reply">
+            <div class="tweet-inner">
+                <img src="${reply.profilePic}" class="profile-pic">
+                    <div>
+                        <p class="handle">${reply.handle}</p>
+                        <p class="tweet-text">${reply.tweetText}</p>
+                    </div>
+                </div>
         </div>
-</div>
-`;
+        `;
       });
     }
 
     feedHtml += `
-<div class="tweet">
-    <div class="tweet-inner">
-        <img src="${tweet.profilePic}" class="profile-pic">
-        <div>
-            <p class="handle">${tweet.handle}</p>
-            <p class="tweet-text">${tweet.tweetText}</p>
-            <div class="tweet-details">
-                <span class="tweet-detail">
-                    <i class="fa-regular fa-comment-dots"
-                    data-reply="${tweet.uuid}"
-                    ></i>
-                    ${tweet.replies.length}
-                </span>
-                <span class="tweet-detail">
-                    <i class="fa-solid fa-heart ${likeIconClass}"
-                    data-like="${tweet.uuid}"
-                    ></i>
-                    ${tweet.likes}
-                </span>
-                <span class="tweet-detail">
-                    <i class="fa-solid fa-retweet ${retweetIconClass}"
-                    data-retweet="${tweet.uuid}"
-                    ></i>
-                    ${tweet.retweets}
-                </span>
-            </div>   
-        </div>            
+    <div class="tweet">
+        <div class="tweet-inner">
+            <img src="${tweet.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${tweet.handle}</p>
+                <p class="tweet-text">${tweet.tweetText}</p>
+                <div class="tweet-details">
+                    <span class="tweet-detail">
+                        <i class="fa-regular fa-comment-dots"
+                        data-reply="${tweet.uuid}"
+                        ></i>
+                        ${tweet.replies.length}
+                    </span>
+                    <span class="tweet-detail">
+                        <i class="fa-solid fa-heart ${likeIconClass}"
+                        data-like="${tweet.uuid}"
+                        ></i>
+                        ${tweet.likes}
+                    </span>
+                    <span class="tweet-detail">
+                        <i class="fa-solid fa-retweet ${retweetIconClass}"
+                        data-retweet="${tweet.uuid}"
+                        ></i>
+                        ${tweet.retweets}
+                    </span>`;
+    if (tweet.isMine) {
+      feedHtml += `
+        <span class="tweet-detail">            
+            <i class="fa-solid fa-trash" 
+            data-delete="${tweet.uuid}"
+            ></i>
+        </span>
+        `;
+    }
+    feedHtml += `
+                </div>   
+            </div>            
+        </div>
+        <div class="hidden" id="replies-${tweet.uuid}">
+            ${repliesHtml}
+        </div>   
     </div>
-    <div class="hidden" id="replies-${tweet.uuid}">
-        ${repliesHtml}
-    </div>   
-</div>
-`;
+    `;
   });
   return feedHtml;
 }
