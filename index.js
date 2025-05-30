ab2b35ad30517eef51ff418f14190fa95e11d406
Mirko <ruhlmirkojoel@gmail.com>
2025-05-30 00:54:03 -0300

    Changed index li element render with javascript to add more in the future with just one word

---
+++ index.js
@@ -0,0 +1,35 @@
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const websitesFolder = ["./websites/", "index.html"];
+const websitesData = [
+  "counter",
+  "calculator",
+  "digitalCard",
+  "amazonClone",
+  "spaceX",
+  "GIFts",
+  "homeTown",
+  "basketball",
+  "blackjack",
+  "extension",
+  "googleClone",
+  "unitConverter",
+  "Oldagram",
+  "cookieConsent",
+  "catMemePicker",
+  "twiterClone",
+];
+
+const websitesUl = document.getElementById("websites-ul");
+const htmlStr = websitesData
+  .map((website) => {
+    let websiteLi = `<li><a href="${websitesFolder[0]}${website}/${
+      websitesFolder[1]
+    }">${capitalize(website)}</li>`;
+    return websiteLi;
+  })
+  .join("");
+
+websitesUl.innerHTML = htmlStr;
