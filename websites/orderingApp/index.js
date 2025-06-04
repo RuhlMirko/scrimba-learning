63ce67f6a642b1aaf84e952cab891517c9728ba2
Mirko <ruhlmirkojoel@gmail.com>
2025-06-04 13:31:52 -0300

    Added functionality to add to cart buttons, now they update the checkout section with items and their corresponding price

--- websites/orderingApp/index.js
+++ websites/orderingApp/index.js
@@ -1,22 +1,41 @@
 import { itemsArray } from "./data.js";
 const itemsEL = document.getElementById("items");
+const checkoutEL = document.getElementById("checkout");
 
-let strHtml = "";
-for (let i of itemsArray) {
-  strHtml += `
-    <div class="product" >
-        <img src="${i.image}" alt="" />
-        <p>${i.name}</p>
-        <h3>USD $${i.price}</h3>
-        <button data-id=${i.id}>Add to cart</button>
-    </div>
-`;
+function renderProducts() {
+  let strHtml = "";
+  for (let i of itemsArray) {
+    strHtml += `
+        <div class="product" >
+            <img src="${i.image}" alt="" />
+            <p>${i.name}</p>
+            <h3>USD $${i.price}</h3>
+            <button data-id=${i.id}>Add to cart</button>
+        </div>
+    `;
+  }
+  itemsEL.innerHTML = strHtml;
 }
 
 document.addEventListener("click", function (e) {
-  addToCart(e.target.dataset.id);
+  if (e.target.dataset.id) {
+    addToCart(e.target.dataset.id);
+  }
 });
 
-const addToCart = function (id) {};
+const addToCart = function (id) {
+  const targetObj = itemsArray.filter(function (item) {
+    return item.id == id;
+  })[0];
+  checkoutEL.innerHTML += `
+    <div class="item">
+        <div class="item-desc">
+        <div>${targetObj.name}</div>
+        <button>remove</button>
+        </div>
+        <div>USD $${targetObj.price}</div>
+    </div>
+  `;
+};
 
-itemsEL.innerHTML = strHtml;
+renderProducts();
