5d6888110ef69154fbcd7fd24eb7849b370f107c
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-07-06 19:28:11 -0300

    Added clipboard function
    Clicking any value from the generated scheme will now clip said value to the device clipboard

--- websites/colorScheme/app.js
+++ websites/colorScheme/app.js
@@ -3,16 +3,15 @@ const schemeSelect = document.getElementById("scheme-selector");
 const hexcodesContainer = document.getElementById("color-scheme-hexcodes");
 const displayContainer = document.getElementById("color-scheme-display");
 const baseColor = document.getElementById("base-color-picker");
+let buttons = document.querySelectorAll("#color-scheme-hexcodes button");
 
 getSchemeBtn.addEventListener("click", () => {
   const targetHex = baseColor.value.slice(1, 7).toUpperCase();
-  console.log(schemeSelect.value, targetHex);
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${targetHex}&mode=${schemeSelect.value}&count=5`
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.colors);
       let displayHtml = "";
       let btnHtml = "";
       for (let color of data.colors) {
@@ -26,5 +25,21 @@ getSchemeBtn.addEventListener("click", () => {
 
       displayContainer.innerHTML = displayHtml;
       hexcodesContainer.innerHTML = btnHtml;
+      buttons = document.querySelectorAll("#color-scheme-hexcodes button");
+
+      buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+          let strClip = button.textContent;
+          navigator.clipboard.writeText(strClip).then(() => {
+            console.log(strClip);
+          });
+        });
+      });
     });
 });
+
+const copyToClipboard = (text) => {
+  navigator.clipboard.writeText(text).then(() => {
+    console.log("Copied to clipboard: " + text);
+  });
+};
