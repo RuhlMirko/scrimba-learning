cbd1b9b96b60eb8d170a4faf688f61051a91824f
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-10-03 00:50:56 -0300

    Added my first react page

---
+++ first-react/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// function App() {
+//   return (    
+//     <ol>
+//       <li>React</li>
+//       <li>Javas</li>
+//       <li>Python</li>
+//     </ol>    
+//   )
+// }
+
+const App = () => (    
+    <ol>
+      <li>React</li>
+      <li>Javas</li>
+      <li>Python</li>
+    </ol>    
+  )
+
+
+export default App
