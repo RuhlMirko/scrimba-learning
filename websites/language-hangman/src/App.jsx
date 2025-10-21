d021ec8a6469b554978b78e35f546824c0fe021c
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-10-21 01:03:01 -0300

    Added clsx
    Added clsx to conditianlly render right and wrong answers

--- websites/language-hangman/src/App.jsx
+++ websites/language-hangman/src/App.jsx
@@ -1,10 +1,31 @@
 import { useState } from "react"
+import { clsx } from "clsx"
 import { languages } from "./languages"
+
+/**
+ * Goal: Allow the user to start guessing the letters
+ * 
+ * Challenge: Update the keyboard when a letter is right
+ * or wrong.
+ * 
+ * Bonus: use the `clsx` package to easily add conditional 
+ * classNames to the keys of the keyboard. Check the docs 
+ * to learn how to use it 📖
+ */
 
 export default function AssemblyEndgame() {
     const [currentWord, setCurrentWord] = useState("react")
-    
+    const [guessedLetters, setGuessedLetters] = useState([])
+
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    function addGuessedLetter(letter) {
+        setGuessedLetters(prevLetters =>
+            prevLetters.includes(letter) ?
+                prevLetters :
+                [...prevLetters, letter]
+        )
+    }
 
     const languageElements = languages.map(lang => {
         const styles = {
@@ -21,14 +42,32 @@ export default function AssemblyEndgame() {
             </span>
         )
     })
-    
+
     const letterElements = currentWord.split("").map((letter, index) => (
         <span key={index}>{letter.toUpperCase()}</span>
     ))
-    
-    const keyboardElements = alphabet.split("").map(letter => (
-        <button key={letter}>{letter.toUpperCase()}</button>
-    ))
+
+    const keyboardElements = alphabet.split("").map(letter => {
+        const isGuessed = guessedLetters.includes(letter)
+        const isCorrect = isGuessed && currentWord.includes(letter)
+        const isWrong = isGuessed && !currentWord.includes(letter)
+        const className = clsx({
+            correct: isCorrect,
+            wrong: isWrong
+        })
+        
+        console.log(className)
+        
+        return (
+            <button
+                className={className}
+                key={letter}
+                onClick={() => addGuessedLetter(letter)}
+            >
+                {letter.toUpperCase()}
+            </button>
+        )
+    })
 
     return (
         <main>
