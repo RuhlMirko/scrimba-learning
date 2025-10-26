28641538e9046dfcb64641aa3ece64d0792cee2d
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-10-25 22:21:30 -0300

    Add Quizzical app UI and initial styles
    Introduces the Quizzical app landing page with a title, description, and start button.

--- websites/quizzReact/quizzApp/src/components/App.jsx
+++ websites/quizzReact/quizzApp/src/components/App.jsx
@@ -4,9 +4,11 @@ import './styles.css'
 function App() {  
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <main>
+      <h1>Quizzical</h1>
+      <p>Welcome to an interactive version of the "<a href="https://opentdb.com/">Open Trivia Database</a>". Click the button below to start a new game.</p>
+      <button>Start Quizz</button>
+    </main>
   )
 }
 
