8bf63b2cbad92cc90b74b81692822a37bed50bb4
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-11-24 13:00:11 -0300

    Add new React + Vite project: bento-grids
    Initial commit for the bento-grids project using React and Vite. Includes project setup files, configuration, source code, assets, and documentation. Also updates index.css in quizzReact/quizzApp.

---
+++ websites/bento-grids/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {  
+
+  return (
+    <body className='grid-container'>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>        
+    </body>
+  )
+}
+
+export default App
