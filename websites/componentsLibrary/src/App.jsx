2bfbd121de6cd0b8cc21de2e40ee61ff58c017bf
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-11-28 19:51:53 -0300

    Initial commit of Project
    Setup using Vite and React, added initial files for basic styling and a template for the rest of components

---
+++ websites/componentsLibrary/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import '../index.css'
+
+function App() {
+
+  return (
+    <body>      
+      <header>
+        <h1>Scrimba components library challenges</h1>  
+      </header>
+
+      <main>
+        <section>
+          <h2>Badges</h2>
+          <p>These badges have either a pill or square shape. It only supports some color choices which are:</p>
+          <ul>
+            <li>Grey</li>  
+            <li>Red</li>
+            <li>Yellow</li>
+            <li>Green</li>
+            <li>Blue</li>
+            <li>Indigo</li>
+            <li>Purple</li>
+            <li>Pink</li>            
+          </ul>
+        </section>
+
+      </main>
+      
+      
+    </body>
+  )
+}
+
+export default App
