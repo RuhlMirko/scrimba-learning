61431f21143e02e811aa637d0dd0adc0bbac7d58
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-12-11 12:20:59 -0300

    Add nested routes and components for Host Van detail
    Implemented nested routing for /host/vans/:id with Details, Pricing, and Photos subroutes.

--- websites/vans/pages/Host/HostVanDetail.jsx
+++ websites/vans/pages/Host/HostVanDetail.jsx
@@ -1,25 +1,79 @@
 import React from "react"
-import { useParams } from "react-router-dom"
+import { useParams, Link, NavLink, Outlet } from "react-router-dom"
 
 export default function HostVanDetail() {
-
-    /**
-     * Challenge (not optional!): build the shared UI portion of the
-     * Host Van Detail page. This is
-     * 
-     * Optional portion: also style it to look like the design.
-     * 
-     * For now, get the data from a request to `/api/host/vans/:id`
-     * and display the van image, name, price, type
-     */
     const { id } = useParams()
     const [currentVan, setCurrentVan] = React.useState(null)
+
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
 
     React.useEffect(() => {
         fetch(`/api/host/vans/${id}`)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setCurrentVan(data.vans))
     }, [])
 
-    return <h1>Host Van Detail Page</h1>
+    if (!currentVan) {
+        return <h1>Loading...</h1>
+    }
+    return (
+        <section>
+            <Link
+                to=".."
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to all vans</span></Link>
+
+            <div className="host-van-detail-layout-container">
+                <div className="host-van-detail">
+                    <img src={currentVan.imageUrl} />
+                    <div className="host-van-detail-info-text">
+                        <i
+                            className={`van-type van-type-${currentVan.type}`}
+                        >
+                            {currentVan.type}
+                        </i>
+                        <h3>{currentVan.name}</h3>
+                        <h4>${currentVan.price}/day</h4>
+                    </div>
+                </div>
+
+                <nav className="host-van-detail-nav">
+                    <NavLink
+                        to="."
+                        end
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Details
+                    </NavLink>
+                    <NavLink
+                        to="pricing"
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Pricing
+                    </NavLink>
+                    <NavLink
+                        to="photos"
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Photos
+                    </NavLink>
+                </nav>
+                <Outlet context={{ currentVan }} />
+            </div>
+        </section>
+    )
 }
+
+/**
+ * Challenge: check out the docs linked in the slide, and see if you
+ * can implement the Outlet Context feature it talks about.
+ * 
+ * Part of this challenge will require you to (finally) build out those
+ * nested components. Again, if you don't need CSS practice, you can
+ * skip the styling part, and I'll handle that for you.
+ */
