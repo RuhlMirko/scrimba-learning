fd76e2c72be1609dc334c61df59743d849bc4c3c
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-12-17 01:34:27 -0300

    Refactor to family tree React components
    Removed product-related components and data, and introduced a new component hierarchy (App, GrandParent, Parent, Child, GrandChild) to demonstrate React rendering and component structure. Updated index.jsx and index.html to use the new App component. Cleaned up unused files and dependencies.

--- websites/perfomanceReact/index.jsx
+++ websites/perfomanceReact/index.jsx
@@ -1,57 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Product from "./Product"
-import productsData from "./data"
+import App from "./App"
 
-function App() {
-  const [count, setCount] = React.useState(0)
-  const [sort, setSort] = React.useState(false)
-
-  function increment() {
-    setCount(prevCount => prevCount + 1)
-  }
-
-  function decrement() {
-    setCount(prevCount => prevCount - 1)
-  }
- 
-  const startTime2 = Date.now()
-  
-  const sortedProducts = React.useMemo(()=>{
-    return [...productsData].sort(
-    (a, b) => a.name.localeCompare(b.name)
-  )
-  },[sort])
-  
-  const endTime2 = Date.now()
-  console.log(`Took ${endTime2 - startTime2}ms`)
-
-  const visibleProducts = sort ? sortedProducts : productsData
-
-  return (
-    <>
-      <h1>The current count is {count}</h1>
-      <button className="button" onClick={decrement}>-</button>
-      <button className="button" onClick={increment}>+</button>
-      <br />
-      <br />
-      <button
-        className="button"
-        onClick={() => setSort(prev => !prev)}
-      >
-        {sort ? "Unsort" : "Sort"}
-      </button>
-      <br />
-      <br />
-      <div className="products-list">
-        {
-          visibleProducts.map(product => (
-            <Product key={product.id} product={product} />
-          ))
-        }
-      </div>
-    </>
-  )
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />); 
