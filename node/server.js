1f9658a43add7a196e24493610dfbd61419dd134
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-04-17 01:17:59 -0300

    Added json responses

--- node/server.js
+++ node/server.js
@@ -1,10 +1,30 @@
 import http from "node:http";
+import { getDataFromDB } from "./database/db.js";
 
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/api") {
-    res.end("This is from the server");
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB();
+
+  if (req.url === "/api" && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.end(JSON.stringify(destinations));
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 404;
+    res.end(
+      JSON.stringify({
+        error: "not found",
+        message: "The requested route does not exist",
+      }),
+    );
+    /*
+Challenge:
+  1. If the client tries to access a route that isn’t covered by the above, send this object: 
+      {error: "not found", message: "The requested route does not exist"}
+  Think: what do we need to send along with the data?
+*/
   }
 });
 
