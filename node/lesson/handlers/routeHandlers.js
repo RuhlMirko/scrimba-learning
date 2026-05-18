2d83e99f004ddca83d257fbb569669ddd50bd59d
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-05-18 05:36:26 -0300

    Added solution to post part 2

--- node/lesson/handlers/routeHandlers.js
+++ node/lesson/handlers/routeHandlers.js
@@ -1,25 +1,24 @@
 import { getData } from '../utils/getData.js'
 import { sendResponse } from '../utils/sendResponse.js'
-// import { parseJSONBody } from '../utils/parseJSONBody.js'
+import { parseJSONBody } from '../utils/parseJSONBody.js'
+// import { addNewSighting } from '../utils/addNewSighting.js'
 
 export async function handleGet(res) {
   const data = await getData()
   const content = JSON.stringify(data)
   sendResponse(res, 200, 'application/json', content)
-}  
+}
 
-export function handlePost(req, res) {
-/*
-Challenge 2:
-  1. Create a const 'rawBody' to store whatever is returned by parseJSONBody()
-  2. For now, log 'rawBody'.
-  3. Input an entry on the front end to test.
-*/
+export async function handlePost(req, res) {
 
-} 
+  try {
+    const parsedBody = await parseJSONBody(req)
+    await addNewSighting(parsedBody)
+    sendResponse(res, 201, 'application/json', JSON.stringify(parsedBody))
+  } catch (err) {
+    sendResponse(res, 400, 'application/json', JSON.stringify({error: err}))
+  }
 
-// santizeData() 
-// addNewSighting() will do the donkey work of adding the data to our dataset
-// sendResponse()
+}
 
 
