86f79ce377ca4eda0072d24adee4654f2478dae9
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-05-19 15:03:11 -0300

    Added sanitized data handler

--- node/lesson/utils/addNewSighting.js
+++ node/lesson/utils/addNewSighting.js
@@ -1,25 +1,30 @@
-function addNewSighting(newSighting) {
+import path from 'node:path'
+import fs from 'node:fs/promises'
+import { getData } from './getData.js'
 
-  try {
-/*
-    1. Get the existing data (remember, this will already be a JS array)
-    2. Push the new sighting to it
-    3. Write data to the file.
-    4. Add the new sighting and check out the 'read' page.
-    5. Throw an error (in the catch block) if any of 1-3 above fail.
+export async function addNewSighting(newSighting) {
+
+  try { 
+
+    const sightings = await getData()
+    sightings.push(newSighting)
     
-    To write data:
-    Import fs/promises
-    Use the writeFile method with the following:
-      - the relative path to the file 
-      - The data (what should we do to this data first?)
-      - The encoding 'utf8'
+    const pathJSON = path.join('data', 'data.json')
+    
+    await fs.writeFile(
+      pathJSON,
+      JSON.stringify(sightings, null, 2),
+      'utf8'
+    )
+/*
+
+    4. Add the new sighting and check out the 'read' page.
     
     Bonus: figure out how to prettify the JSON!
     Remember to uncomment the import statement in routeHandler.js!!
 */
   } catch (err) {
-
+    throw new Error(err)
   }
 
 }
