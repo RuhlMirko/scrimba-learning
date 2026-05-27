cdbe7806389f4e79be79003cb6a0418c100b29b8
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-05-27 14:06:59 -0300

    Updated alternative testimonial styles

--- websites/componentsLibrary/src/Component/Testimonial.jsx
+++ websites/componentsLibrary/src/Component/Testimonial.jsx
@@ -16,8 +16,11 @@ export default function Testimonial({children, name, ocuppation, picture}) {
     )
     else return (
         <div className="info-container no-picture">
-            <h3>Work<span>cation</span></h3>            
-            <p>"{children}"</p>            
+            <div className="body">
+                <h3>Work<span>cation</span></h3>            
+                <p>"{children}"</p>                
+            </div>
+            
             <footer>
                 <h3>{name}</h3>
                 <h4>Workcation, {ocuppation}</h4>            
